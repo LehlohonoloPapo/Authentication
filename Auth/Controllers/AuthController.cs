10c1710afa099ca380db27d0341b27ee9e638e7d
LehlohonoloPapo <narepapo.12@gmail.com>
2023-03-21 08:52:25 +0200

    first Commit

---
+++ Auth/Controllers/AuthController.cs
@@ -0,0 +1,37 @@
+﻿using Auth.Models;
+using Auth.Services;
+using Microsoft.AspNetCore.Mvc;
+
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Auth.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        // GET: api/<AuthController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<AuthController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<AuthController>
+        [HttpPost]
+        public AuthResponse Post([FromBody] AuthRequest authRequest)
+        {
+            return new AuthService().GetUser(authRequest);
+        }
+
+
+    }
+}
---
+++ Auth/Services/AuthService.cs
@@ -0,0 +1,24 @@
+﻿using Auth.Models;
+
+namespace Auth.Services
+{
+    public  class AuthService
+    {
+        private readonly string _dbConfig;//= config;
+
+
+        public  AuthResponse GetUser(AuthRequest authRequest)
+        {
+            if(authRequest == null)
+            {
+                throw new ArgumentNullException(nameof(authRequest));
+            }
+            var n = new AuthResponse();
+            n.FirstName = authRequest.UserName;
+            n.Username = "Admin";
+
+            return  n;
+
+        }
+    }
+}
